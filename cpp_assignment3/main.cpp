bc04bae0d3f8032a06bf8f8d0d32bd1961343955
JessiDG <feelingelephants@gmail.com>
2018-04-24 15:59:58 -0700

    Initial Commit

---
+++ cpp_assignment3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_assignment3
+//
+//  Created by Keladry on 4/24/18.
+//  Copyright © 2018 Keladry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
